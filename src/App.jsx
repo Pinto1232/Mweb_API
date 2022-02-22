178d93dc08c9f051bbf48550e7a22e56f4915380
Pinto Manuel <pintoydc@gmail.com>
2022-02-23 01:08:25 +0200

    Commit new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
+import React from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <div className="App">
-       <h2>Hello World this i my app</h2>
+        <h2>Hello World</h2>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
