f4fbd3164112139a3a79ac65d3e5bd7f27e80d10
Pinto Manuel <pintoydc@gmail.com>
2022-02-23 01:13:48 +0200

    Publish changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css'
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div className="App">
-        <h2>Hello World</h2>
+        <Navbar />
     </div>
   );
 };
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className="nav">
+            <h2>I am novbar</h2>
+        </div>
+    );
+};
+
+export default Navbar;
