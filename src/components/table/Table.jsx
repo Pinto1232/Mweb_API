583c95d98a322b48ad1d718969fdbfb6a819167e
Pinto Manuel <pintoydc@gmail.com>
2022-02-25 00:14:46 +0200

    style done

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -9,31 +9,31 @@ return (
     <Card className="container">
         <div className="input-content__items">
             <label>
-                <input type="radio" name="choice" value="Java" /> Learn JavaScript
+                <input type="radio" name="choice" value="Java" />R0 - R699
             </label>
         </div>
 
         <div className="input-content__items">
             <label>
-                <input type="radio" name="choice" value="Java" /> Learn JavaScript
+                <input type="radio" name="choice" value="Java" /> R700 - R999
             </label>
         </div>
 
         <div className="input-content__items">
             <label>
-                <input type="radio" name="choice" value="Java" /> Learn JavaScript
+                <input type="radio" name="choice" value="Java" /> R1000
             </label>
         </div>
 
         <div className="input-content__items">
             <label>
-                <input type="radio" name="choice" value="Java" /> Learn JavaScript
+                <input type="radio" name="choice" value="Java" /> R20000
             </label>
         </div>
 
         <div className="input-content__items">
             <label>
-                <input type="radio" name="choice" value="Java" /> Learn JavaScript
+                <input type="radio" name="choice" value="Java" /> R3000+
             </label>
         </div>
     </Card>
