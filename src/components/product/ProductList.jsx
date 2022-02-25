db70da3e00df830b6b85b57b225281b64b98f19a
Pinto Manuel <pintoydc@gmail.com>
2022-02-25 17:35:43 +0200

    I am now getting the fiber products

--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
-import { useEffect } from 'react';
-import { useFetch } from '../hooks/useFetch';
 import ProductCard from '../UI-Product/ProductCard';
 import './ProductList.css'
 
 const ProductList = () => {
-    console.log('Allo product');
-    const {data: products, isFetching} =
-            useFetch('/prod/baas/proxy/marketing/products/promos/FTTH-LINKAFRICA-SETUP-CLAWBACK-100MBUP,FTTH-WEBCONNECT-M2M,FTTH-FROG-M2M-SETUP-CLAWBACK-100MBUP,FTTH-MFN-SETUP-CLAWBACK-50DEAL,FTTH-LIGHTSTRUCK-SETUP-CLAWBACK-100MBUP,FTTH-OPEN-SETUP-CLAWBACK-100MBUP-NEW,FTTH-EVOTEL-CLAWBACK-100MBUP,FTTH-LINKLAYER-CLAWBACK-100MBUP,FTTH-VODA-CLAWBACK-100MBUP,FTTH-TTCONNECT-CLAWBACK-100MBUP,FTTH-CLEARACCESS-CLAWBACK,FTTH-ZOOM-CLAWBACK,FTTH-OCTOTEL-SETUP-100MBUP,FTTH-CCC-CLARA-CLAWBACK,FTTH-CCC-SETUP-100MBUP,FTTH-CCC-ALT-SETUP-100MBUP,FTTH-VUMA-CLAWBACK-100MBUP?sellable_online=true')
-    console.log(products); 
+    const [product, setProduct] = useState([])
+
+    function fetchProductHandler(event){
+        event.preventDefault()
+        fetch('https://apigw.mweb.co.za/prod/baas/proxy/marketing/products/promos/FTTH-LINKAFRICA-SETUP-CLAWBACK-100MBUP,FTTH-WEBCONNECT-M2M,FTTH-FROG-M2M-SETUP-CLAWBACK-100MBUP,FTTH-MFN-SETUP-CLAWBACK-50DEAL,FTTH-LIGHTSTRUCK-SETUP-CLAWBACK-100MBUP,FTTH-OPEN-SETUP-CLAWBACK-100MBUP-NEW,FTTH-EVOTEL-CLAWBACK-100MBUP,FTTH-LINKLAYER-CLAWBACK-100MBUP,FTTH-VODA-CLAWBACK-100MBUP,FTTH-TTCONNECT-CLAWBACK-100MBUP,FTTH-CLEARACCESS-CLAWBACK,FTTH-ZOOM-CLAWBACK,FTTH-OCTOTEL-SETUP-100MBUP,FTTH-CCC-CLARA-CLAWBACK,FTTH-CCC-SETUP-100MBUP,FTTH-CCC-ALT-SETUP-100MBUP,FTTH-VUMA-CLAWBACK-100MBUP?sellable_online=true')
+        .then(response => {
+            return response.json()
+        }).then(data => {
+           setProduct(data.products);
+            console.log(data);
+        })
+    }
+  
     
 
    
@@ -17,7 +24,7 @@ return (
     <ProductCard className="prod-card">
         <h2>Fibre Products Providers</h2>
         <div className="container-product">
-            <a href="">Show Product</a>
+            <a href="#" onClick={fetchProductHandler}>Show Product</a>
             <p>
                 
             </p>
