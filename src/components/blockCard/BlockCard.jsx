f8dc7054ccf293f919a1c475472d0b8ac27cfeec
Pinto Manuel <pintoydc@gmail.com>
2022-02-25 18:53:31 +0200

    Latest commit

--- src/components/blockCard/BlockCard.jsx
+++ src/components/blockCard/BlockCard.jsx
@@ -1,53 +1,66 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Block from '../UI-Cards/Block'
 import Card from '../UI/Card'
-import React from 'react';
+import React, { useState } from 'react';
 import './BlockCard.css'
 import PriceHeading from '../headingPrice/PriceHeading';
 
 const BlockCard = (props) => {
-return (
-<div className="card-grid">
-<div className="container">
-           <Card className="menu">
-                <ul>
-                    <li>
-                        <a href="#">Filter By:</a>
+    const [price, setPrice] = useState([])
+
+    function fetchPriceHandler(event) {
+        event.preventDefault()
+        fetch('https://apigw.mweb.co.za/prod/baas/proxy/marketing/products/promos/FTTH-LINKAFRICA-SETUP-CLAWBACK-100MBUP,FTTH-WEBCONNECT-M2M,FTTH-FROG-M2M-SETUP-CLAWBACK-100MBUP,FTTH-MFN-SETUP-CLAWBACK-50DEAL,FTTH-LIGHTSTRUCK-SETUP-CLAWBACK-100MBUP,FTTH-OPEN-SETUP-CLAWBACK-100MBUP-NEW,FTTH-EVOTEL-CLAWBACK-100MBUP,FTTH-LINKLAYER-CLAWBACK-100MBUP,FTTH-VODA-CLAWBACK-100MBUP,FTTH-TTCONNECT-CLAWBACK-100MBUP,FTTH-CLEARACCESS-CLAWBACK,FTTH-ZOOM-CLAWBACK,FTTH-OCTOTEL-SETUP-100MBUP,FTTH-CCC-CLARA-CLAWBACK,FTTH-CCC-SETUP-100MBUP,FTTH-CCC-ALT-SETUP-100MBUP,FTTH-VUMA-CLAWBACK-100MBUP?sellable_online=true')
+            .then(response => {
+                return response.json()
+            }).then(data => {
+                setPrice(data.products);
+                console.log(data);
+        })
+    }
+        
+    return (
+        <div className="card-grid">
+           <div className="container">
+                <Card className="menu">
                         <ul>
-                            <li><a href="#">Price</a></li>
+                            <li>
+                                <a href="#">Filter By:</a>
+                                <ul>
+                                    <li><a href="#" onClick={fetchPriceHandler}>Price</a></li>
+                                </ul>
+                            </li>
                         </ul>
-                    </li>
-                </ul>
-            </Card>
-           </div>
-    <div className="container">
-        <Block className="block-card__info">
-            <h2> card card</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo officiis assumenda.
-            </p>
-            <a href="#">Read more</a>
-        </Block>
+                    </Card>
+                </div>
+            <div className="container">
+                <Block className="block-card__info">
+                    <h2> card card</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo officiis assumenda.
+                    </p>
+                    <a href="#">Read more</a>
+                </Block>
 
-        <Block className="block-card__info">
-            <h2><img src="" alt="" /> div card</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo officiis assumenda.
-            </p>
-            <a href="#">Read more</a>
-        </Block>
+                <Block className="block-card__info">
+                    <h2><img src="" alt="" /> div card</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo officiis assumenda.
+                    </p>
+                    <a href="#">Read more</a>
+                </Block>
 
-        <Block className="block-card__info">
-            <h2><img src="" alt="" /> div card</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo officiis assumenda.
-            </p>
-            <a href="#">Read more</a>
-        </Block>
+                <Block className="block-card__info">
+                    <h2><img src="" alt="" /> div card</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo officiis assumenda.
+                    </p>
+                    <a href="#">Read more</a>
+                </Block>
+                </div>
+                <PriceHeading />
         </div>
-        <PriceHeading />
-</div>
-);
+    );
 };
 
 export default BlockCard;
--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -3,35 +3,38 @@ import ProductCard from '../UI-Product/ProductCard';
 import './ProductList.css'
 
 const ProductList = () => {
-    const [product, setProduct] = useState([])
+const [product, setProduct] = useState([])
 
-    function fetchProductHandler(event){
-        event.preventDefault()
-        fetch('https://apigw.mweb.co.za/prod/baas/proxy/marketing/products/promos/FTTH-LINKAFRICA-SETUP-CLAWBACK-100MBUP,FTTH-WEBCONNECT-M2M,FTTH-FROG-M2M-SETUP-CLAWBACK-100MBUP,FTTH-MFN-SETUP-CLAWBACK-50DEAL,FTTH-LIGHTSTRUCK-SETUP-CLAWBACK-100MBUP,FTTH-OPEN-SETUP-CLAWBACK-100MBUP-NEW,FTTH-EVOTEL-CLAWBACK-100MBUP,FTTH-LINKLAYER-CLAWBACK-100MBUP,FTTH-VODA-CLAWBACK-100MBUP,FTTH-TTCONNECT-CLAWBACK-100MBUP,FTTH-CLEARACCESS-CLAWBACK,FTTH-ZOOM-CLAWBACK,FTTH-OCTOTEL-SETUP-100MBUP,FTTH-CCC-CLARA-CLAWBACK,FTTH-CCC-SETUP-100MBUP,FTTH-CCC-ALT-SETUP-100MBUP,FTTH-VUMA-CLAWBACK-100MBUP?sellable_online=true')
+function fetchProductHandler(event) {
+    event.preventDefault()
+    fetch('https://apigw.mweb.co.za/prod/baas/proxy/marketing/products/promos/FTTH-LINKAFRICA-SETUP-CLAWBACK-100MBUP,FTTH-WEBCONNECT-M2M,FTTH-FROG-M2M-SETUP-CLAWBACK-100MBUP,FTTH-MFN-SETUP-CLAWBACK-50DEAL,FTTH-LIGHTSTRUCK-SETUP-CLAWBACK-100MBUP,FTTH-OPEN-SETUP-CLAWBACK-100MBUP-NEW,FTTH-EVOTEL-CLAWBACK-100MBUP,FTTH-LINKLAYER-CLAWBACK-100MBUP,FTTH-VODA-CLAWBACK-100MBUP,FTTH-TTCONNECT-CLAWBACK-100MBUP,FTTH-CLEARACCESS-CLAWBACK,FTTH-ZOOM-CLAWBACK,FTTH-OCTOTEL-SETUP-100MBUP,FTTH-CCC-CLARA-CLAWBACK,FTTH-CCC-SETUP-100MBUP,FTTH-CCC-ALT-SETUP-100MBUP,FTTH-VUMA-CLAWBACK-100MBUP?sellable_online=true')
         .then(response => {
             return response.json()
         }).then(data => {
-           setProduct(data.products);
+            setProduct(data.products);
             console.log(data);
         })
-    }
-  
-    
+}
 
-   
-return (
-<div className="container">
-    <ProductCard className="prod-card">
-        <h2>Fibre Products Providers</h2>
-        <div className="container-product">
-            <a href="#" onClick={fetchProductHandler}>Show Product</a>
-            <p>
-                
-            </p>
+    return (
+        <div className="container">
+            <ProductCard className="prod-card">
+                <h2>Fibre Products Providers</h2>
+                <div className="container-product">
+                    <a href="#" onClick={fetchProductHandler}>Show Product</a>
+                </div>
+            </ProductCard>
+            <br />
+            <ProductCard className="prod-card">
+                <div className="container-product">
+
+                    <p>
+
+                    </p>
+                </div>
+            </ProductCard>
         </div>
-    </ProductCard>
-</div>
-);
-};
+        );
+    };
 
 export default ProductList;
