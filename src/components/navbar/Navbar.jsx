63f42d6d7f91001098a684e946fef226362dc5b9
Pinto Manuel <pintoydc@gmail.com>
2022-03-05 01:55:31 +0200

    Adding css module in the navbar button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from '../UI/Card';
 import './Navbar.css'
+import styles from './Navbar.module.css'
 
 const Navbar = (props) => {
     return (
@@ -11,6 +12,7 @@ const Navbar = (props) => {
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Packages</a></li>
                     <li><a href="#">Contact</a></li>
+                    <li><a className={styles.Login} href="#">Login</a></li>
                 </ul>
             </div>
         </Card>
